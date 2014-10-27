b20fdd9a955e80dad3f052f81886498418df38fa
Weizhen Wang <ww2339@columbia.edu>
2014-10-27 00:14:41 -0400

    Add getpoints servlet.

---
+++ src/getpoints.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class getpoints
+ */
+public class getpoints extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public getpoints() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String keyword = request.getParameter("keyword");
+		String startTime = request.getParameter("start");
+		String endTime = request.getParameter("end");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+        out.println(keyword + " " + startTime + " " + endTime);
+	}
+
+}
